3c3b0e2272ba43709d51c0a10c8c1507752e0bbf
Nikhil2072001 <116430126+Nikhil2072001@users.noreply.github.com>
2022-12-25 12:09:13 +0530

    msg

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -27,22 +27,20 @@ this.calendar=new ArrayList<>();
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-//int c=1;
-
-int n = calendar.size();
-
-        ArrayList<pair> list=new ArrayList<>();
-        for(int i=0;i<n;i++)
-        {
-            list.add(new pair(calendar.get(i).getStartTime(),calendar.get(i).getEndTime()));
+        int cnt = 1;
+        ArrayList<pair> list = new ArrayList<>();
+        for(int i=0; i<calendar.size(); i++){
+            Meeting m = calendar.get(i);
+            list.add(new pair(m.getStartTime(), m.getEndTime()));
         }
         Collections.sort(list);
-        int cnt=1;
-        LocalTime prev=list.get(0).end;
-        for(int i=1;i<n;i++) {
-            if(list.get(i).strt.compareTo(prev)>0)   {
+        LocalTime temp = list.get(0).end;
+        for(int i=1; i<list.size(); i++){
+            pair p = list.get(i);
+            if(p.strt.compareTo(temp) > 0){
                 cnt++;
-                prev=list.get(i).end; }
+                temp = p.end;
+            }
         }
         return cnt;
     }
